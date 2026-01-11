78b34bc1734516e7d7f7c2aea75a9867368904fe
Ryxena <rubbenme@proton.me>
2026-01-11 10:42:32 +0700

    Update: remove emoji shit

--- public/seed.php
+++ public/seed.php
@@ -3,8 +3,6 @@ require_once '../app/init.php';
 
 $db = new Database;
 
-echo "<h3>🌱 Mulai Seeding (1 Admin & 1 Jamaah)...</h3><hr>";
-
 try {
     $passAdmin = password_hash('admin123', PASSWORD_DEFAULT); // Password: admin123
 
@@ -17,11 +15,11 @@ try {
     $db->bind('nama', 'Administrator Utama');
     $db->execute();
 
-    echo "<p style='color:green'>✅ <b>Sukses!</b> Admin dibuat.<br>
+    echo "<p style='color:green'><b>Sukses!</b> Admin dibuat.<br>
           Username: <b>admin</b> | Password: <b>admin123</b></p>";
 
 } catch (Exception $e) {
-    echo "<p style='color:red'>❌ <b>Skip Admin:</b> Username 'admin' mungkin sudah ada.</p>";
+    echo "<p style='color:red'><b>Skip Admin:</b> Username 'admin' mungkin sudah ada.</p>";
 }
 
 echo "<hr>";
@@ -41,11 +39,11 @@ try {
     $db->bind('alamat', 'Jl. Testing No. 1, Jakarta');
     $db->execute();
 
-    echo "<p style='color:green'>✅ <b>Sukses!</b> Jamaah dibuat.<br>
+    echo "<p style='color:green'><b>Sukses!</b> Jamaah dibuat.<br>
           Email: <b>user@sobatgurun.com</b> | Password: <b>jamaah123</b></p>";
 
 } catch (Exception $e) {
-    echo "<p style='color:red'>❌ <b>Skip Jamaah:</b> Email/NIK tersebut mungkin sudah ada.</p>";
+    echo "<p style='color:red'><b>Skip Jamaah:</b> Email/NIK tersebut mungkin sudah ada.</p>";
 }
 
 echo "<hr><b>Selesai.</b> Silakan hapus file ini jika sudah live.";
